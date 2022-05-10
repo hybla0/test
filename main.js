2ad1d757062cda5fee2fcfef1012e1f4b5cffde5
hybla0 <58774420+hybla0@users.noreply.github.com>
2022-05-10 20:15:36 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,22 +1,12 @@
-import process from 'process'
-import minimist from 'minimist'
 import { Web3Storage, getFilesFromPath } from 'web3.storage'
 
-async function main () {
-  const args = minimist(process.argv.slice(2))
-  const token = args.token
+const token = process.env.API_TOKEN
+const client = new Web3Storage({ token })
 
-  if (!token) {
-    console.error('A token is needed. You can create one on https://web3.storage')
-    return
-  }
-
-  const storage = new Web3Storage({ token })
-
-  const files = await getFilesFromPath('./iso.iso')
-  const cid = await storage.put(files)
-
-  console.log('Content added with CID:', cid)
+async function storeFiles () {
+  const files = await getFilesFromPath("../iso.iso")
+  const cid = await client.put(files)
+  console.log(cid)
 }
 
-main()
+storeFiles()
